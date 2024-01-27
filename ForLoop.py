a48ff86d2a943fe7381975b016f42e7c8cae1652
tfxTanoli <usman5194999@gmail.com>
2024-01-27 10:47:21 +0500

    For Loop Iterate Over A List

--- ForLoop.py
+++ ForLoop.py
@@ -1,3 +1,11 @@
 num = int(input("Enter The Number : \n"))
 for i in range(num):
     print(i)
+
+
+prices = [10 , 20, 30]
+sumOfPrices = 0
+for item in prices:
+    sumOfPrices = sumOfPrices + item
+
+print("Sum = ",sumOfPrices)
