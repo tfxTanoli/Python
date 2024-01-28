b8d9c1ad7253c531ea3f81ec65a6affd3b93eb03
tfxTanoli <usman5194999@gmail.com>
2024-01-28 08:07:09 +0500

    Dictionary Exercise

--- Dictionary.py
+++ Dictionary.py
@@ -8,3 +8,19 @@ print(studentInfo)
 
 # studentInfo.popitem()
 # print(studentInfo)
+
+phone = input("Phone : ")
+output = ""
+digitsMapping = {
+    "1":"One",
+    "2" : "Two",
+    "3" : "Three",
+    "4" : "Four"
+}
+
+for ch in phone:
+    if ch in digitsMapping:
+        print(digitsMapping.get(ch))
+    # output += digitsMapping.get(ch, "!")
+
+# print(output+"")
