b705c49908234273a0d4494fe2a06d7cc93a2bac
tfxTanoli <usman5194999@gmail.com>
2024-01-28 07:45:31 +0500

    Remove Duplicates From List

---
+++ UnPacking.py
@@ -0,0 +1,5 @@
+#UnPacking applies on tuples and also on lists
+#only we have to do is to exchange the paranthesis with square brackets
+coordinates = (1 , 2, 3)
+x , y ,z = coordinates
+print(x*y*z)
